2726ac96a5288e3a1a1740d4b0e81db1e7ade8db
gmarsano <gstnmarsano@gmail.com>
2019-07-08 12:22:31 -0400

    se agrega fecha al primer parrafo de la pagina por javascript

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var d = new Date();
+var weekDays = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+var months = [
+  "Ene", "Feb", "Mar", "Abr", "May", "Jun",
+  "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"
+];
+var minutes = d.getMinutes() > 9 ? d.getMinutes() : ("0" + d.getMinutes());
+var seconds = d.getSeconds() > 9 ? d.getSeconds() : ("0" + d.getSeconds());
+
+document.querySelectorAll("p:first-child")[0].innerHTML += " <strong>[" +
+  //nombre-dia, numero-dia nombre-mes año-completo hora
+  weekDays[d.getDay()] + ", " + d.getDate() + " de " + months[d.getMonth()] +
+  " de " + d.getFullYear() + " " + d.getHours() + ":" + minutes + ":" + seconds
+  + "]</strong>";
